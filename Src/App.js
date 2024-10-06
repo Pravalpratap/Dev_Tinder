e42e10bd606d84952cf225643dfd0d1f876fc7f1
Praval <pravalp949@gmail.com>
2024-10-06 10:35:09 +0530

    added dynamic data using post

--- Src/App.js
+++ Src/App.js
@@ -1,23 +1,22 @@
 const express = require("express");
 const app = express();
 const { connectDB } = require("../Config/database");
-const User = require("../Model/user");
+const User=require("../Model/user");
 
+
+
+app.use(express.json());
 app.post("/signup", async (req, res) => {
-  const user = new User({
-    firstName: "praval pratap",
-    lastName: "singh",
-    emailId: "pravalp949@gmail.com",
-    password: "praval@123",
-  });
- try{
-  await user.save();
-  res.send("user added succesfully");
- }
- catch(err){{
-  res.status(400).send("Error saving the user" ,+ err.message);
- }}
- 
+  // console.log("Request body:", req.body);
+
+  const user = new User(req.body);
+
+  try {
+    await user.save();
+    res.send("User added successfully");
+  } catch (err) {
+    res.status(400).send("Error saving the user: " + err.message); // Adjusted error handling
+  }
 });
 
 // Start the server
